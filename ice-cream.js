0758856a255a88fc76d8427c0fc0bc155039f546
Princeton Arulnesan <princeton.arul@gmail.com>
2020-04-15 00:25:34 +0000

    ice cream example

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+// first task 
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+//second task
+console.log(iceCreamFlavours[0]);
+//third task
+console.log(iceCreamFlavours[5]);
+//fourth task
+console.log(iceCreamFlavours.length);
